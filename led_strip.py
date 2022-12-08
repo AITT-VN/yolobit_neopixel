a6f337252bfd89fcaf5ae3cbef8c1338a4530df5
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-12-08 13:40:53 +0700

    Update led_strip.py

--- led_strip.py
+++ led_strip.py
@@ -2,9 +2,6 @@ import math, random, neopixel,time
 import machine, _thread
 from machine import Pin, ADC, DAC
 
-NUM_PIXELS = 30
-Pin_NP = 22
-
 class Led_Strip:
     def __init__(self,Pin_NP,NUM_PIXELS):
         self.Pin_NP = Pin_NP
@@ -17,25 +14,44 @@ class Led_Strip:
         return tuple(int(color[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
     def set_hex_color(self,color):
-        for j in range(NUM_PIXELS):
+        for j in range(self.NUM_PIXELS):
             self.np[j] = self.hex_to_rgb(color)
             self.np.write()
 
     def set_all(self,color):
-        for j in range(NUM_PIXELS):
+        for j in range(self.NUM_PIXELS):
             self.np[j] = color
             self.np.write()
 
     def wipe_effect(self):
-        for i in range(NUM_PIXELS):
+        for i in range(self.NUM_PIXELS):
             self.np[i] = (255, 255, 255)
             self.np.write()
             time.sleep_ms(100)
-        for i in range(NUM_PIXELS):
+        for i in range(self.NUM_PIXELS):
             self.np[i] = (0, 0, 0)
             self.np.write()
             time.sleep_ms(100)
 
+    def show_index_led(self, index, color, delay=None):
+
+      if index == 0:
+            for j in range(self.NUM_PIXELS):
+                self.np[j] = color
+            self.np.write()
+      elif (index > 0) and (index <= self.NUM_PIXELS) :
+            self.np[index - 1] = color
+            self.np.write()
+      if delay != None:
+            time.sleep(delay)
+            if index == 0:
+                for j in range(self.NUM_PIXELS):
+                    self.np[j] = (0, 0, 0)
+                self.np.write()
+            elif (index > 0) and (index <= self.NUM_PIXELS) :
+                self.np[index - 1] = (0, 0, 0)
+                self.np.write()
+    
     def dim_effect(self):
         RED = 0
         GREEN = 0
@@ -77,8 +93,8 @@ class Led_Strip:
 
     def twinkle_effect(self):
         for k in range(3):  
-            for i in range (30):
-                pixel = random.randint(0, 29)
+            for i in range (self.NUM_PIXELS):
+                pixel = random.randint(0, self.NUM_PIXELS - 1)
                 Random = random.randint(0, 255)
                 self.np[pixel] = (Random, Random, Random)
                 self.np.write()
@@ -88,8 +104,8 @@ class Led_Strip:
 
     def spakle_effect(self):
         for k in range(2):
-            for i in range (10):
-                pixel = random.randint(1, 29)
+            for i in range (self.NUM_PIXELS):
+                pixel = random.randint(1, self.NUM_PIXELS - 1)
                 self.np[pixel] = (255, 255, 255)
                 self.np.write()  
                 time.sleep_ms(100)
@@ -99,12 +115,12 @@ class Led_Strip:
     def theaterChase_effect(self):
         for j in range(10):
             for q in range(3):
-                for i in range(0, NUM_PIXELS, 3):
-                    self.np[i+q] = (255,0,0)
+                for i in range(0, self.NUM_PIXELS, 3):
+                    self.np[i-q] = (255,0,0)
                 self.np.write()
-                time.sleep_ms(50)
-                for i in range(0, NUM_PIXELS, 3):
-                    self.np[i+q] = (0,0,0)
+                time.sleep_ms(100)
+                for i in range(0, self.NUM_PIXELS, 3):
+                    self.np[i-q] = (0,0,0)
 
     def bounce_effect(self):
         n = self.np.n
@@ -120,29 +136,29 @@ class Led_Strip:
 
     def firework_effect(self):
         for i in range(20,5,-5):
-            for j in range(NUM_PIXELS-i):
+            for j in range(self.NUM_PIXELS-i):
                 self.np[j] = (150+(j*5),100+(j*5),50+(j*5))
                 self.np.write()
                 time.sleep_ms(300)
             color = (0,0,0)
             self.set_all(color)
-            self.np[NUM_PIXELS-i] = (255,255,255)
-            self.np[NUM_PIXELS-(i+2)] = (0,255,0)
-            self.np[NUM_PIXELS-(i+1)] = (0,255,0)
-            self.np[NUM_PIXELS-(i-2)] = (0,255,0)    
-            self.np[NUM_PIXELS-(i-1)] = (0,255,0)
+            self.np[self.NUM_PIXELS-i] = (255,255,255)
+            self.np[self.NUM_PIXELS-(i+2)] = (0,255,0)
+            self.np[self.NUM_PIXELS-(i+1)] = (0,255,0)
+            self.np[self.NUM_PIXELS-(i-2)] = (0,255,0)    
+            self.np[self.NUM_PIXELS-(i-1)] = (0,255,0)
             self.np.write()
             time.sleep_ms(200)
-            self.np[NUM_PIXELS-(i+4)] = (255,255,0)
-            self.np[NUM_PIXELS-(i+3)] = (255,255,0)
-            self.np[NUM_PIXELS-(i-4)] = (255,255,0)
-            self.np[NUM_PIXELS-(i-3)] = (255,255,0)
+            self.np[self.NUM_PIXELS-(i+4)] = (255,255,0)
+            self.np[self.NUM_PIXELS-(i+3)] = (255,255,0)
+            self.np[self.NUM_PIXELS-(i-4)] = (255,255,0)
+            self.np[self.NUM_PIXELS-(i-3)] = (255,255,0)
             self.np.write()
             time.sleep_ms(200)
-            self.np[NUM_PIXELS-(i+6)] = (0,0,255)
-            self.np[NUM_PIXELS-(i+5)] = (0,0,255)
-            self.np[NUM_PIXELS-(i-6)] = (0,0,255)
-            self.np[NUM_PIXELS-(i-5)] = (0,0,255)
+            self.np[self.NUM_PIXELS-(i+6)] = (0,0,255)
+            self.np[self.NUM_PIXELS-(i+5)] = (0,0,255)
+            self.np[self.NUM_PIXELS-(i-6)] = (0,0,255)
+            self.np[self.NUM_PIXELS-(i-5)] = (0,0,255)
             self.np.write()
             time.sleep_ms(1000)
             color = (0,0,0)
@@ -161,8 +177,8 @@ class Led_Strip:
 
     def rainbow_effect(self):
         for j in range(255):
-            for i in range(NUM_PIXELS):
-                rc_index = (i * 256 // NUM_PIXELS) + j
+            for i in range(self.NUM_PIXELS):
+                rc_index = (i * 256 // self.NUM_PIXELS) + j
                 self.np[i] = self.wheel(rc_index & 255)
             self.np.write()
             time.sleep_ms(1)
@@ -193,3 +209,5 @@ class Led_Strip:
     def clear(self):
         color = (0,0,0)
         self.set_all(color)
+
+
